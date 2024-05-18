45fff07951dad3cb397ff535bf24b0ee2c74b63c
didoslavov <didkoslawow@gmail.com>
2024-05-18 18:12:53 +0300

    add custom input component

---
+++ adv-components/src/components/Input.tsx
@@ -0,0 +1,21 @@
+import { ComponentPropsWithoutRef } from 'react';
+
+type InputProps = {
+    id: string;
+    label: string;
+} & ComponentPropsWithoutRef<'input'>;
+
+const Input = ({ id, label, ...props }: InputProps) => {
+    return (
+        <p className="flex flex-col gap-2 font-bold text-sm uppercase">
+            <label htmlFor={id}>{label}</label>
+            <input
+                id={id}
+                {...props}
+                className="px-2 py-3 outline-none focus:outline-lime-950 focus:-outline-offset-4 rounded-md bg-lime-100 text-lime-950 font-medium"
+            />
+        </p>
+    );
+};
+
+export default Input;
