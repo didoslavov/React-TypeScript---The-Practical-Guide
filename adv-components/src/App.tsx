25869f009cef5eb2ba2b85ecdc96af275f0d8b42
didoslavov <didkoslawow@gmail.com>
2024-05-18 18:13:06 +0300

    add custom button component

--- adv-components/src/App.tsx
+++ adv-components/src/App.tsx
@@ -1,7 +1,22 @@
-import './App.css';
+import Button from './components/Button';
+import Input from './components/Input';
 
 function App() {
-    return <h1>Hello</h1>;
+    return (
+        <main className="bg-lime-50 h-screen pt-20 font-poppins">
+            <section className="max-w-[768px] bg-lime-700 text-lime-50 rounded-md py-10 px-6 mx-auto">
+                <div className="flex flex-col gap-4">
+                    <Input id="name" label="Your Name" />
+                    <p>
+                        <Button>A button</Button>
+                    </p>
+                    <p>
+                        <Button href="https://google.com">A link</Button>
+                    </p>
+                </div>
+            </section>
+        </main>
+    );
 }
 
 export default App;
---
+++ adv-components/src/components/Button.tsx
@@ -0,0 +1,22 @@
+import { ComponentPropsWithoutRef } from 'react';
+
+type ButtonProps = ComponentPropsWithoutRef<'button'> & {
+    href?: never;
+};
+
+type LinkProps = ComponentPropsWithoutRef<'a'> & {
+    href?: string;
+};
+
+const isAnchor = (props: ButtonProps | LinkProps): props is LinkProps => 'href' in props;
+
+const Button = (props: ButtonProps | LinkProps) => {
+    const baseStyles = 'py-2 px-4 bg-lime-200 hover:bg-lime-300 transition-colors duration-300 text-lime-950 rounded-md';
+    return isAnchor(props) ? (
+        <a {...props} className={`${baseStyles} underline`}></a>
+    ) : (
+        <button {...props} className={`${baseStyles}`}></button>
+    );
+};
+
+export default Button;
