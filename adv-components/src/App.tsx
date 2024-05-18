599e9c7f36402a399fa4bd7bd1e627691b7f8700
didoslavov <didkoslawow@gmail.com>
2024-05-19 00:20:06 +0300

    add forward ref to input

--- adv-components/src/App.tsx
+++ adv-components/src/App.tsx
@@ -1,12 +1,14 @@
+import { useRef } from 'react';
 import Button from './components/Button';
 import Input from './components/Input';
 
 function App() {
+    const inputRef = useRef<HTMLInputElement>(null);
     return (
         <main className="bg-lime-50 h-screen pt-20 font-poppins">
             <section className="max-w-[768px] bg-lime-700 text-lime-50 rounded-md py-10 px-6 mx-auto">
                 <div className="flex flex-col gap-4">
-                    <Input id="name" label="Your Name" />
+                    <Input id="name" label="Your Name" ref={inputRef} />
                     <p>
                         <Button>A button</Button>
                     </p>
--- adv-components/src/components/Input.tsx
+++ adv-components/src/components/Input.tsx
@@ -1,21 +1,22 @@
-import { type ComponentPropsWithoutRef } from 'react';
+import { forwardRef, type ComponentPropsWithoutRef } from 'react';
 
 type InputProps = {
     id: string;
     label: string;
 } & ComponentPropsWithoutRef<'input'>;
 
-const Input = ({ id, label, ...props }: InputProps) => {
+const Input = forwardRef<HTMLInputElement, InputProps>(({ id, label, ...props }, ref) => {
     return (
         <p className="flex flex-col gap-2 font-bold text-sm uppercase">
             <label htmlFor={id}>{label}</label>
             <input
+                ref={ref}
                 id={id}
                 {...props}
                 className="px-2 py-3 outline-none focus:outline-lime-950 focus:-outline-offset-4 rounded-md bg-lime-100 text-lime-950 font-medium"
             />
         </p>
     );
-};
+});
 
 export default Input;
