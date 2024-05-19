1c6d921c74a84fd7e97aaecf61f8d8ba6b064de4
didoslavov <didkoslawow@gmail.com>
2024-05-19 16:08:33 +0300

    add custom hooks for redux store

--- redux-ecommerce/src/store/store.ts
+++ redux-ecommerce/src/store/store.ts
@@ -4,3 +4,6 @@ import { cartSlice } from './cart-slice';
 export const store = configureStore({
     reducer: { cart: cartSlice.reducer },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
