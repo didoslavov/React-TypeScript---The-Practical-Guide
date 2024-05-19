4904de14bb4d163a7d20a43e104ec5c587f09d53
didoslavov <didkoslawow@gmail.com>
2024-05-19 04:29:58 +0300

    add add timer component

---
+++ adv-components/src/components/AddTimer.tsx
@@ -0,0 +1,30 @@
+import { useRef } from 'react';
+import Form, { FormHandle } from './ui/Form';
+import Input from './ui/Input';
+import Button from './ui/Button';
+import { useTimersContext } from '../store/timers-context';
+
+const AddTimer = () => {
+    const form = useRef<FormHandle>(null);
+    const { addTimer } = useTimersContext();
+
+    const handleSaveTimer = (data: unknown) => {
+        const formData = data as { name: string; duration: string };
+
+        addTimer({ id: Date.now(), name: formData.name, duration: Number(formData.duration) });
+
+        form.current?.clear();
+    };
+
+    return (
+        <Form ref={form} onSave={handleSaveTimer} className="w-1/2 mx-auto text-center">
+            <Input id="name" label="Name" type="text" className="mb-4" />
+            <Input id="duration" label="Duration" type="number" className="mb-4" />
+            <p className="text-center mb-8">
+                <Button>Add Timer</Button>
+            </p>
+        </Form>
+    );
+};
+
+export default AddTimer;
