e9b42992f10aa30892a0f9b2593387b49e6065f6
didoslavov <didkoslawow@gmail.com>
2024-05-19 04:30:47 +0300

    add header component

---
+++ adv-components/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import { useTimersContext } from '../store/timers-context';
+import Button from './ui/Button';
+
+const Header = () => {
+    const timersCtx = useTimersContext();
+
+    return (
+        <header className="flex bg-lime-700 max-w-[768px] mx-auto justify-between py-10 px-10 rounded-tl-md rounded-tr-md text-lime-50 mt-20">
+            <h1 className="text-2xl font-semibold">ReactTimer</h1>
+            <Button onClick={timersCtx.isRunning ? timersCtx.stopTimer : timersCtx.startTimer}>
+                {timersCtx.isRunning ? 'Stop' : 'Start'} Timers
+            </Button>
+        </header>
+    );
+};
+
+export default Header;
