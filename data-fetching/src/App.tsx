6c4a29add832eca5a19b07c54ba4300910d001cb
didoslavov <didkoslawow@gmail.com>
2024-05-19 14:32:24 +0300

    add data fetching function

--- data-fetching/src/App.tsx
+++ data-fetching/src/App.tsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from 'react';
+import { get } from './util/http';
+import BlogPosts, { BlogPost } from './components/BlogPosts';
+import fetchingImg from './assets/data-fetching.png';
+
+type RawPost = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+};
+
 function App() {
-    return <h1>Hello</h1>;
+    const [data, setData] = useState<BlogPost[]>();
+
+    useEffect(() => {
+        get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+            setData(
+                (response as RawPost[]).map((post) => ({
+                    id: post.id,
+                    title: post.title,
+                    text: post.body,
+                }))
+            );
+        });
+    }, []);
+
+    return (
+        <main className="max-w-[768px] mx-auto pt-10">
+            <img
+                src={fetchingImg}
+                alt="Data fetching image"
+                className="max-w-28 rounded-full mx-auto ring-2 ring-zinc-200 border-zinc-200"
+            />
+            <BlogPosts posts={data || []} />
+        </main>
+    );
 }
 
 export default App;
